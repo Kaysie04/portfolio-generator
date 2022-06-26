3f25e45fddf5b97c55ec471ad84c461309195d94
Kaysie <kaysielandersonwork@gmail.com>
2022-06-26 16:46:22 -0500

    alot

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
  
+//const inquirer = require('inquirer');
 import inquirer from 'inquirer';
-import fs from 'fs';
-//const generatePage = require('./src/page-template');
+import fs from 'fs'
+import generatePage from './src/page-template.js';
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 
 const promptUser = () => {
     return inquirer .prompt([
@@ -148,7 +142,14 @@ const promptUser = () => {
    promptUser()
    .then(promptProject)
    .then(portfolioData => {
-     console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+     fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw err;
+
+       console.log('Portfolio complete! Check out index.html to see the output!');
+    });
+    
    });
    
 
